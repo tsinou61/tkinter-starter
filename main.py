1297ae10ee701907eeccb4d5c822b319fe6a2fc4
Trevor <60404098+tsinou61@users.noreply.github.com>
2020-01-29 14:34:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,27 @@
 
 from tkinter import * # import Tkinter library
 window = Tk()         # Create the application window
-
-# Add a label with the text "Hello"
-lbl = Label(window, text="I am now going to mess up this code!")
-
-# Place the label in the window at 0, 0
+window.title("TrevorApp - Still Better than Safari!")
+window.geometry('1500x500')
+from tkinter.ttk import *
+combo = Combobox(window)
+combo['values']= (1, 2, 3, 4, 5, "Text")
+combo.current(1)
+combo.grid(column=0, row=3)
+lbl = Label(window, text="Hello", font=("Arial Bold", 50))
 lbl.grid(column=0, row=0)
+second = Label(window, text="Type your name and press the button!", font=("Times New Roman", 20))
+second.grid(column=0, row=1)
+def clicked():
+    res = "Welcome to TrevorApp " + txt.get()
+    lbl.configure(text= res)
+txt = Entry(window,width=10)
+txt.grid(column=3, row=0)
+btn = Button(window, text="Click", command=clicked)
+btn.grid(column=2, row=0)
+txt.focus()
+
+
 
 
 window.mainloop()     # Keep the window open
